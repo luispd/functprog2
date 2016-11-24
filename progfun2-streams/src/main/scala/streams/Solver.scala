e9a5981b78b7ed88705206f7fdea2d5004c5b3fb
Luis <megacosmic@hotmail.com>
2016-11-25 00:06:24 +0100

    neighborsWithHistory y newNeighborsOnly

--- progfun2-streams/src/main/scala/streams/Solver.scala
+++ progfun2-streams/src/main/scala/streams/Solver.scala
@@ -10,7 +10,7 @@ trait Solver extends GameDef {
   /**
    * Returns `true` if the block `b` is at the final position
    */
-  def done(b: Block): Boolean = ???
+  def done(b: Block): Boolean = b.isStanding && b.b1 == goal
 
   /**
    * This function takes two arguments: the current block `b` and
@@ -28,7 +28,13 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = ???
+  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = {
+    val nbs = b.legalNeighbors
+    val res = for {
+      n <- nbs
+    } yield (n._1,n._2::history)
+    res.toStream
+  }
 
   /**
    * This function returns the list of neighbors without the block
@@ -36,7 +42,9 @@ trait Solver extends GameDef {
    * make sure that we don't explore circular paths.
    */
   def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
-                       explored: Set[Block]): Stream[(Block, List[Move])] = ???
+                       explored: Set[Block]): Stream[(Block, List[Move])] = {
+    neighbors.filter(p => !explored.contains(p._1))
+  }
 
   /**
    * The function `from` returns the stream of all possible paths
